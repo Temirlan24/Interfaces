0d9bbf453d0980bebe4da47b12f5e9ca1f4e40ca
Temirlan24 <52044447+Temirlan24@users.noreply.github.com>
2019-08-07 11:42:27 +0600

    Добавьте файлы проекта.

---
+++ PracticWork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
